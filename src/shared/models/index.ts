9f202693f15e437a54e635dd31bc8f2a281bd1a0
Girish Nandgawe <girish.nandgawe@gmail.com>
2023-03-05 05:59:10 -0800

    Delete src directory

--- src/shared/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { User } from "./user";
-export { Task } from "./task";
-export { Message } from "./message";
-export { Notification } from "./notification";
-export { Severity, Log } from "./log";
