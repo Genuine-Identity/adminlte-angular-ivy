c18c89c6a2041a1c28d664552d602024058d43c4
Girish Nandgawe <girish.nandgawe@gmail.com>
2023-03-05 06:08:20 -0800

    Add files via upload

---
+++ src/app/shared/components/header/header.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
+  declarations: [
+    HeaderComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    RouterModule
+  ]
+})
+export class HeaderModule { }
